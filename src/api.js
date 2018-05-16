975222f4b5617ef8bf4c7fc80578b2dbe350130f
Jonas Kaltenbach <jonas.kaltenbach@googlemail.com>
2018-05-16 20:26:52 +0200

    removed example code and data

--- src/api.js
+++ src/api.js
@@ -1,16 +1,14 @@
-"use strict";
+'use strict';
 
-const express    = require('express');
+const express = require('express');
 const bodyParser = require('body-parser');
-const helmet     = require('helmet');
+const helmet = require('helmet');
 
 const middlewares = require('./middlewares');
 
-const auth  = require('./routes/auth');
-const movie = require('./routes/movie');
+const auth = require('./routes/auth');
 
 const api = express();
-
 
 // Adding Basic Middlewares
 api.use(helmet());
@@ -18,18 +16,14 @@ api.use(bodyParser.json());
 api.use(bodyParser.urlencoded({ extended: false }));
 api.use(middlewares.allowCrossDomain);
 
-
 // Basic route
 api.get('/', (req, res) => {
-    res.json({
-        name: 'SEBA Master Movie Backend'
-    });
+  res.json({
+    name: 'Communly Backend'
+  });
 });
 
 // API routes
-api.use('/auth'  , auth);
-api.use('/movies', movie);
-
-
+api.use('/auth', auth);
 
 module.exports = api;
