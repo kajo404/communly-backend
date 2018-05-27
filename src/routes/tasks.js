0f6ffa1e392e4431fc9f87523e39add715752748
Jonas Kaltenbach <jonas.kaltenbach@googlemail.com>
2018-05-27 15:58:36 +0200

    removed unsave calls

--- src/routes/tasks.js
+++ src/routes/tasks.js
@@ -6,12 +6,4 @@ const router = express.Router();
 const middlewares = require('../middlewares');
 const TasksController = require('../controllers/tasks');
 
-router.post('/', middlewares.checkAuthentication, TasksController.create);
-router.get('/:id', middlewares.checkAuthentication, TasksController.getAll);
-router.delete(
-  '/:id',
-  middlewares.checkAuthentication,
-  TasksController.deleteById
-);
-
 module.exports = router;
