4d44af763bd07e95462d5cc01babb2bab15ff1dd
Jonas Kaltenbach <jonas.kaltenbach@googlemail.com>
2018-06-10 20:46:24 +0200

    crash fix

--- src/controllers/user.js
+++ src/controllers/user.js
@@ -7,7 +7,7 @@ const TaskListModel = require('../models/taskList');
 const AnnouncementModel = require('../models/announcement');
 
 const getAll = (req, res) => {
-  UserModel.find({}, 'firstname', 'lastname')
+  UserModel.find({}, 'firstname lastname')
     .exec()
     .then(users => {
       res.status(200).json({
